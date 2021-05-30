56f0e33f7a225fe629753babc0f813f45e398f0a
sebastianRaiquenParisi <sebastianrparisi@gmail.com>
2021-05-30 20:30:33 -0300

    sequelize models inicio

---
+++ database/migrations/20210530231203-create-movie.js
@@ -0,0 +1,48 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Movies', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      created_at: {
+        type: Sequelize.DATE
+      },
+      updated_at: {
+        type: Sequelize.DATE
+      },
+      title: {
+        type: Sequelize.STRING
+      },
+      rating: {
+        type: Sequelize.FLOAT
+      },
+      awards: {
+        type: Sequelize.INTEGER
+      },
+      release_date: {
+        type: Sequelize.DATE
+      },
+      length: {
+        type: Sequelize.INTEGER
+      },
+      genre_id: {
+        type: Sequelize.INTEGER
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Movies');
+  }
+};
---
+++ database/models/movie.js
@@ -0,0 +1,31 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Movie extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  };
+  Movie.init({
+    created_at: DataTypes.DATE,
+    updated_at: DataTypes.DATE,
+    title: DataTypes.STRING,
+    rating: DataTypes.FLOAT,
+    awards: DataTypes.INTEGER,
+    release_date: DataTypes.DATE,
+    length: DataTypes.INTEGER,
+    genre_id: DataTypes.INTEGER
+  }, {
+    sequelize,
+    tableName:"movies",
+    modelName: 'Movie',
+  });
+  return Movie;
+};
